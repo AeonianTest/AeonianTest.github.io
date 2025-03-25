d7f153499dcd8a2827b46b9d8adc71a87f9e2ab0
AeonianTest <s4006011@student.rmit.edu.au>
2025-03-25 23:54:32 +1100

    templates

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../css/components/Header.css'
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header
