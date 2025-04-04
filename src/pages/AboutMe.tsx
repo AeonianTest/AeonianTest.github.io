8e4f8c345caf31d446257cafab76056df8ee12d1
AeonianTest <s4006011@student.rmit.edu.au>
2025-04-04 22:43:32 +1100

    About me page template

---
+++ src/pages/AboutMe.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const AboutMe: React.FC = () => {
+
+    
+    return (
+        <div className="OuterContainer">       
+            <>
+                About me
+            </>
+        </div>
+    )
+}
+
+export default AboutMe;
