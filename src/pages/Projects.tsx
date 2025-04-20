f548dde82bc1fdbff6aafdb01ef91d5dd2efa205
AeonianTest <s4006011@student.rmit.edu.au>
2025-04-20 17:28:45 +1000

    Create models + test data

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,17 +1,47 @@
 import '../css/pages/Projects.css'
 import React from 'react';
 
+// Model for an image and its data
+interface ProjectImage {
+    url: string;
+    altText: string;
+}
+
+// Model for Project object
 interface Project {
     id: number;
     title: string;
     description: string;
     techStack: string[];
     repoLink: string;
-} // May need images
+    liveLink?: string;
+    projectImage?: ProjectImage;
+}
 
-const Projects: React.FC = () => {
+const Projects: React.FC = () => {    
+    // Source of data to accomodate website limitations
     const ProjectsList: Project[] = [
-        
+        {
+            id: 1,
+            title: "Title1",
+            description: "Des1",
+            techStack: ["Tech1"],
+            repoLink: "https://location.com"
+        },
+        {
+            id: 2,
+            title: "Title2",
+            description: "Des2",
+            techStack: ["Tech2"],
+            repoLink: "https://location.com"
+        },
+        {
+            id: 3,
+            title: "Title3",
+            description: "Des3",
+            techStack: ["Tech3"],
+            repoLink: "https://location.com"
+        }
     ]
 
     return (
